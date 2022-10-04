4441bf3643db62b9f8968d92f4ccaa295d78e29d
SB-YounG <SB-YounG@outlook.com>
2022-10-04 12:45:25 +0800

    Q3 Longest Unique Strings

--- Q3_Longest_Unique_Strand/main.c
+++ Q3_Longest_Unique_Strand/main.c
@@ -2,12 +2,12 @@
 /*-*- coding:utf-8 -*-*/
 /***************************************************************************************************
 * File: \main.c                                                                                    *
-* Project: Q4_Longest_Unique_Strand                                                                *
+* Project: Q3_Longest_Unique_Strand                                                                *
 * Created Date: Thursday Sep 29th 2022, 9:37:43 am                                                 *
 * Author: Wenren Muyan                                                                             *
 * Comments:                                                                                        *
 * --------------------------------------------------------------------------------                 *
-* Last Modified: 29/09/2022 09:37:43                                                               *
+* Last Modified: 1/10/2022 09:43:8                                                                 *
 * Modified By: Wenren Muyan                                                                        *
 * --------------------------------------------------------------------------------                 *
 * Copyright (c) 2022 - future Wenren Muyan                                                         *
@@ -17,4 +17,40 @@
 * --------------------------------------------------------------------------------                 *
 ***************************************************************************************************/
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+int lengthOfLongestSubstring(char * s){
+    int len = strlen(s);
+
+    int res = 0, j = 0;
+    for(int i = 0; i < len; i++){
+        int cur = j, flag = 0;
+  
+        while(cur < i){
+            if(s[cur] == s[i]){
+                j = cur + 1;
+                flag = 1;
+                break;
+            }
+            else{
+                cur++;
+            }
+        }
+
+        if(!flag && res < i - j + 1) res = i - j + 1;
+    }
+
+    return res;
+}
+
+int main(){
+    char s[15];
+
+    scanf("%s", s);
+
+    printf("%d", lengthOfLongestSubstring(s));
+
+    return 0;
+}
